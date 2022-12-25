1549dac2d517ce29776de0d4a9adc0cc57a2ed3d
ubakang <ngeneubaka1@gmail.com>
2022-12-25 14:23:10 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import {useState, useEffect} from "react"
+import { MapContainer, TileLayer} from 'react-leaflet'
+import Markerposition from "./markerposition"
+import "leaflet/dist/leaflet.css"
+import arrow from "./images/icon-arrow.svg"
+import background from "./images/pattern-bg.png"
+// import { map } from "leaflet"
+
+
+// https://geo.ipify.org/api/v2/country?apiKey=at_ZzYtUYI9bUwKhiwCVvGNpPxIzVE6h&ipAddress=8.8.8.8
+
+function App() {
+
+  const [address, setAddress] = useState(null);
+  const [ipAddress, setIpAddress] = useState("");
+
+  const checkIpAddress =
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi
+  const checkDomain =
+    /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/
+
+  useEffect(() => {
+    try {
+      const getInitialData = async () => {
+        const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=192.212.174.101`)
+        const data = await res.json()
+        setAddress(data);
+    }
+      getInitialData()
+    } catch (error) {
+      console.trace(error)
+    }
+  }, [])
+
+  async function getEnteredAddress() {
+    const res = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&${
+      checkIpAddress.test(ipAddress)
+      ? `ipAddress=${ipAddress}`
+      : checkDomain.test(ipAddress)
+      ? `domain=${ipAddress}`
+      : ""
+      }`)
+        const data = await res.json()
+        setAddress(data);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    getEnteredAddress();
+    setIpAddress("");
+  }
+
+
+  return (
+    <>
+      <section>
+        <div>
+          <img src={background} alt="" className="w-full h-80 object-cover absolute -z-10"/>
+        </div>
+        <article className="p-8">
+          <h1 className="text-2xl text-center text-white
+          font-bold mb-8 lg:text-3xl">IP Address Tracker</h1>
+
+
+          <form
+            onSubmit={handleSubmit}
+            autoCorrect="off"
+            className="flex justify-center max-w-xl mx-auto">
+            <input
+              type="text"
+              name="ipadrress"
+              id="ipaddress"
+              placeholder="search for any IP Address or domain"
+              required
+              className="py-2 px-4 rounded-l-lg w-full"
+              value={ipAddress}
+              onChange={(e) => setIpAddress(e.target.value) }
+            />
+            <button type="submit" className="bg-black py-4 px-4 hover:opacity-60 rounded-r-lg">
+              <img src={arrow} alt="" />
+            </button>
+          </form>
+        </article>
+        {address && <>
+          <article className="bg-white rounded-lg p-8 mx-8 grid grid-cols-1 
+              gap-8 md:grid-cols-2 lg:grid-cols-4 max-w-6xl xl:mx-auto text-center md:text-left lg:-mb-16 relative" style={{zIndex: 10000}}>
+          <div className="lg:border-r lg:border-slate-400">
+            <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3" >IP ADDRESS</h2>
+            <p className="font-bold text-slate-900 text-lg md:text-xl xl:text-2xl">{address.ip}</p>
+          </div>
+          <div className="lg:border-r lg:border-slate-400">
+            <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3" >Location</h2>
+              <p className="font-bold text-slate-900 text-lg md:text-xl xl:text-2xl">{address.location.city}, {address.location.region}</p>
+          </div>
+          <div className="lg:border-r lg:border-slate-400">
+            <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3" >Timezone</h2>
+              <p className="font-bold text-slate-900 text-lg md:text-xl xl:text-2xl">UTC {address.location.timezone}</p>
+          </div>
+          <div>
+            <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3" >isp</h2>
+              <p className="font-bold text-slate-900 text-lg md:text-xl xl:text-2xl">{address.isp}</p>
+          </div>
+        </article>
+
+        <MapContainer
+          center={[address.location.lat, address.location.lng]}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{height: "100vh", width:"100vw"}}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Markerposition address={address} />
+        </MapContainer>
+        </>}
+      </section>
+    </>
+  );
+}
+
+export default App;
